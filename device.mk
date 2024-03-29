e7ea8a5943c10d55eef4a6666a32a87ed7700d95
bheatleyyy <sugakesshaa@gmail.com>
2024-03-29 14:09:01 +0000

    [REPICK] r5x: Drop Vulkan Renderer
    - We're switching to skiagl renderer
    
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- device.mk
+++ device.mk
@@ -181,7 +181,6 @@ PRODUCT_PACKAGES += \
     libdisplayconfig.system.qti \
     libtinyxml \
     libtinyxml2 \
-    libvulkan \
     memtrack.trinket \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.mapper@2.0.vendor \
