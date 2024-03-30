8a9edbacf2c0ada777d4866729b54f47caa4e261
MAdMiZ <mizdrake7@gmail.com>
2024-03-30 17:32:19 +0000

    r5x: [TEMP] Drop Lineage LiveDisplay
    Signed-off-by: Christopher <mizdrake7@gmail.com>

--- device.mk
+++ device.mk
@@ -341,10 +341,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-# LiveDisplay native
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     android.hardware.media.omx@1.0-impl \
