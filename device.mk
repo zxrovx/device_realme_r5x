a1b7fb0108873d83ae2cefe9fea70fae09076bd4
dianlujitao <dianlujitao@lineageos.org>
2024-03-30 17:28:36 +0000

    r5x: Switch to Audio HAL V7 to V5
    All target devices use pre-R audio.bluetooth_qti.default.so and
    pre-R audio hal, in which the new set_event_callback API is not
    implemented and the audio service will crash due to mismatched struct.
    
    jjpprrrr:
    The audio flinger also warns about event callback when playing
    audio through a USB audio adapter with DAC. Let's go back to v5
    to avoid calling these unimplemented features at all.
    
    ...
    D AudioFlinger: Failed to add event callback
    ...
    
    This reverts commit a05e1447a86b68ab22abb1ee9c0052567adbaf4d.
    
    Change-Id: Ia330ac05d84d3dfb3a639458d66a3dc29689e67c
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: MadhavSaladi <baaswanthmadhav@gmail.com>

--- device.mk
+++ device.mk
@@ -51,8 +51,8 @@ ART_BUILD_HOST_DEBUG := false
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.1-impl \
-    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.effect@2.0-service \
     android.hardware.soundtrigger@2.3-impl \
     android.hardware.audio.service \
