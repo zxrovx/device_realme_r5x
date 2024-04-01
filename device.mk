1024b44129674dc8f28829f1526952689297ccfd
Alexandru Scurtu <sasha.scurtu@gmail.com>
2024-04-01 15:31:37 +0000

    r5x: Build sensorservice 1.0 HAL
    W HidlServiceManagement: Waited one second for android.frameworks.sensorservice@1.0::ISensorManager/default
    I hwservicemanager: Since android.frameworks.sensorservice@1.0::ISensorManager/default is not registered, trying to start it as a lazy HAL.
    I HidlServiceManagement: getService: Trying again for android.frameworks.sensorservice@1.0::ISensorManager/default...
    W libc    : Unable to set property "ctl.interface_start" to "android.frameworks.sensorservice@1.0::ISensorManager/default": error code: 0x20
    
    E init    : Control message: Could not find 'android.frameworks.sensorservice@1.0::ISensorManager/default' for ctl.interface_start from pid: 530 (/system/bin/hwservicemanager)

--- device.mk
+++ device.mk
@@ -538,6 +538,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
     libsensorndkbridge \
+    android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor
 
 PRODUCT_COPY_FILES += \
