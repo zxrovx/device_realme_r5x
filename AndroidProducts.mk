f05c89d3d8339cf47377f35f18cf7d94efb3c91e
ZXROVX <91070944+zxrovx@users.noreply.github.com>
2024-05-16 14:35:28 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2023-2024 The Afterlife Project
+# Copyright (C) 2023-2024 The LineageOs Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/afterlife_r5x.mk
+    $(LOCAL_DIR)/lineage_r5x.mk
 
 COMMON_LUNCH_CHOICES := \
-    afterlife_r5x-user \
-    afterlife_r5x-userdebug \
-    afterlife_r5x-eng
+    lineage_r5x-user \
+    lineage_r5x-userdebug \
+    lineage_r5x-eng
