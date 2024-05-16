e8ee7fe0ab67a8e39925de0df45a151915f8a86d
ZXROVX <91070944+zxrovx@users.noreply.github.com>
2024-05-16 14:46:24 +0700

    Init LineageOS

--- afterlife_r5x.mk
+++ afterlife_r5x.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2023-202 The Afterlife Project
+# Copyright (C) 2023-202 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,8 +18,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Halloween stuff
-$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
+# Inherit some common stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from r5x device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -35,7 +35,7 @@ PRODUCT_MANUFACTURER := Realme
 PRODUCT_NAME := afterlife_r5x
 PRODUCT_MODEL := Realme 5 Series
 
-PRODUCT_GMS_CLIENTID_BASE := android-oppo
+PRODUCT_GMS_CLIENTID_BASE := android-realme
 
 TARGET_VENDOR_PRODUCT_NAME := r5x
 TARGET_VENDOR_DEVICE_NAME := r5x
@@ -43,13 +43,3 @@ TARGET_VENDOR_DEVICE_NAME := r5x
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="r5x" \
     PRIVATE_BUILD_DESC="unknown-user 10 QKQ1.200209.002 unknown release-keys"
-
-# Afterlife Flags
-AFTERLIFE_GAPPS := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-AFTERLIFE_CORE := true
-AFTERLIFE_EXTRA := true
-BUILD_GALLERYGO := true
-BUILD_GMAIL := false
-BUILD_GCALC := true
-AFTERLIFE_MAINTAINER := MAdMiZ
